45645a671fcf7719d826140136c4957fae5d9798
Du_Yi_Wu_Er <61488145+Du-Yi-Wu-Er@users.noreply.github.com>
2020-10-01 10:08:19 +0800

    独一无二：（提交了文件）

--- src/main/java/com/du_yi_wu_er/theendlessseamod/Block/BlockInfusedClayBlock.java
+++ src/main/java/com/du_yi_wu_er/theendlessseamod/Block/BlockInfusedClayBlock.java
@@ -2,6 +2,7 @@ package com.du_yi_wu_er.theendlessseamod.Block;
 
 import com.du_yi_wu_er.theendlessseamod.tab.Tabtheendlessseamod;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
 public class BlockInfusedClayBlock extends Block {
@@ -11,6 +12,7 @@ public class BlockInfusedClayBlock extends Block {
         this.setUnlocalizedName(nama);
         this.setRegistryName(nama);
         this.setHardness(0.5F);
+        this.setSoundType(SoundType.GROUND);
         this.setCreativeTab(Tabtheendlessseamod.tabtheendlessseamod);
     }
 }
