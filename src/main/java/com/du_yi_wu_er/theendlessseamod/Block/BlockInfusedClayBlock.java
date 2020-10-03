227b0eb4d02e0dc405ed46b92290251518576a05
末影君 <1164050766@qq.com>
2020-10-03 20:05:35 +0800

    末影君：提交文件

--- src/main/java/com/du_yi_wu_er/theendlessseamod/Block/BlockInfusedClayBlock.java
+++ src/main/java/com/du_yi_wu_er/theendlessseamod/Block/BlockInfusedClayBlock.java
@@ -1,9 +1,14 @@
 package com.du_yi_wu_er.theendlessseamod.Block;
 
+import com.du_yi_wu_er.theendlessseamod.Item.ItemLoader;
 import com.du_yi_wu_er.theendlessseamod.tab.Tabtheendlessseamod;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+
+import java.util.Random;
 
 public class BlockInfusedClayBlock extends Block {
 
@@ -15,4 +20,13 @@ public class BlockInfusedClayBlock extends Block {
         this.setSoundType(SoundType.GROUND);
         this.setCreativeTab(Tabtheendlessseamod.tabtheendlessseamod);
     }
+
+    public Item getItemDropped(IBlockState state, Random quantity, int quantityDropped){
+        return ItemLoader.infused_clay;
+    }
+
+    public int quantityDropped(Random quantity){
+        return 7 + quantity.nextInt(2);
+    }
+
 }
